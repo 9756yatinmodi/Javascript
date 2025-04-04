e8a59687d96d6efabfb37e113aad1f775c4bb073
yatin modi <9756yatinmodi@gmail.com>
2025-04-04 12:06:39 +0530

    Add files via upload

---
+++ operators/script.js
@@ -0,0 +1,68 @@
+// operators
+
+// arithmatic operator - used to perform mathamatical operation
+// symbol : + , - , * , / , %
+
+// exampel
+
+let a = 10 ;
+let b = 10 ;
+
+ let c = a + b // c = 10 + 10 = 20
+ //  c = a - b // c = 10 - 10 = 0
+ // c = a * b // c = 10 * 10 = 100
+ //  c = a / b // c = 10 / 10 = 1
+ //  c = a % b // c = 10 % 10 = 0
+
+ document.writeln(c)
+
+
+ // assignment operators - used to assign value in varaible
+ // symbol : = , += , -= , *= , /=
+
+ // exampel
+
+ let x = 10; 
+ //  x += x// x = 10 + 10 = 10
+ //  x -= 10 // x = 20 - 10 = 10
+ // x *= x // x = 10 * 10 = 100
+ // x /= x // x = 10 / 10 = 1
+
+
+ document.writeln(x)
+
+ // relanational operators
+ // symbol : > , < , <= , >= , == , != , ===
+
+ // exampel
+
+ let p = 10; 
+ let q = 20 ;
+ 
+ document.writeln(p>q)
+ document.writeln(p<q)
+ document.writeln(p<=q)
+ document.writeln(p>=q)
+ document.writeln(p==q)
+ document.writeln(p!=q)
+ document.writeln(p===q)
+
+// logical operators
+//symbol :  &&(AND) , || (OR) , !
+
+// &&- all condition must be true than final answer will be true
+// || - any one condition true than final answer true
+
+// exampel
+
+let e = 10 ;
+let f = 20;
+ document.writeln(e<=f && e<f) // 10 <= 20 = true && 10<20 = true = true
+// document.writeln(e==f && e<f) // 10 == 20 = false && 10<20 = true = false
+
+
+//let e = 10 ;
+//let f = 20;
+
+// document.writeln(e<=f || e<f) // 10 <= 20 = true && 10<20 = true = true
+// document.writeln(e==f || e<f) // 10 == 20 = false && 10<20 = true = true
