9942433d7c9426aff89c745a64ea9e790a913435
yatin modi <9756yatinmodi@gmail.com>
2025-04-15 18:08:17 +0530

    Add files via upload

---
+++ for_loop/script.js
@@ -0,0 +1,91 @@
+// q1 . 2 1 1/2 1/4 1/8
+// document.writeln(" 1 2")
+// for( let i=2; i<=10; i*=2)
+// {
+//  document.writeln("1/" + i)
+// }
+
+
+// q2 . 7, 10, 8, 11, 9, 12 , 10
+
+// let a = 7
+// let b = 10
+// for(let i=1; i<=4; i++)
+// {
+//     document.writeln(a + " "+b)
+//     a++;
+//     b++;
+// }
+
+//q3 .36, 34, 30, 28, 24, 22
+
+// let a = 36
+// let b = 34
+// for(let i=1; i<=3; i++){
+//     document.writeln(a +" "+b)
+//     a-=6;
+//     b-=6;
+// }
+
+//q4. 22, 21, 23, 22, 24, 23, 25
+// let a = 22;
+// let b = 21
+// for(let i=1; i<=4; i++){
+//     document.writeln(a +" "+b)
+//     a+=1;
+//     b+=1;
+// }
+
+// q5 . 53, 53, 40, 40, 27, 27, 14 , 14
+
+// let a=53
+// for(let i=1; i<=4; i++){
+//     document.writeln(a+" "+a)
+//     a-=13;
+// }
+
+// Q6. 21, 9, 21, 11, 21, 13, 21, 15
+// let a = 9
+// for(let i=1; i<=4; i++){
+//     document.writeln("21" +" "+ a)
+//     a+=2;
+
+// }
+
+//q7. 58, 52, 46, 40, 34,28
+// let a = 58
+// for(let i=1; i<=6; i++){
+//     document.writeln(a)
+//     a-=6;
+
+// }
+
+//q8 . 3, 4, 7, 8, 11, 12 , 15
+// let a = 3
+// for(let i=1; i<=7; i++)
+// {
+//     document.writeln(a)
+//     if(a%2==0){
+//         a+=3
+//     }else{
+//         a+=1
+//     }
+// }
+
+//q9.  8, 22, 8, 28, 8 , 34 , 8 , 40
+// let a = 22
+//  for(let i=1; i<=4; i++){
+//      document.writeln("8" +" "+ a)
+//    a+=6;
+//  }
+
+//q10 . 31, 29, 24, 22, 17
+let a = 31
+for(let i=1; i<=4; i++){
+    document.writeln(a)
+   if(a%2==0){
+        a-=5
+    }else{
+        a-=2
+    }
+}
