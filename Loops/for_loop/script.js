45c0ade8603474e637181bbfe636027592f129ff
yatin modi <9756yatinmodi@gmail.com>
2025-04-23 16:21:33 +0530

    Add files via upload

---
+++ Loops/for_loop/script.js
@@ -0,0 +1,181 @@
+// q1 . 2 1 1/2 1/4 1/8
+// document.writeln(" 1 2")
+// for( let i=2; i<=10; i*=2)
+// {
+//  document.writeln("1/" + i)
+// }
+
+
+// q2 . 7, 10, 8, 11, 9, 12 , 10
+
+// let a = 7
+// let b = 10
+// for(let i=1; i<=4; i++)
+// {
+//     document.writeln(a + " "+b)
+//     a++;
+//     b++;
+// }
+
+//q3 .36, 34, 30, 28, 24, 22
+
+// let a = 36
+// let b = 34
+// for(let i=1; i<=3; i++){
+//     document.writeln(a +" "+b)
+//     a-=6;
+//     b-=6;
+// }
+
+//q4. 22, 21, 23, 22, 24, 23, 25
+// let a = 22;
+// let b = 21
+// for(let i=1; i<=4; i++){
+//     document.writeln(a +" "+b)
+//     a+=1;
+//     b+=1;
+// }
+
+// q5 . 53, 53, 40, 40, 27, 27, 14 , 14
+
+// let a=53
+// for(let i=1; i<=4; i++){
+//     document.writeln(a+" "+a)
+//     a-=13;
+// }
+
+// Q6. 21, 9, 21, 11, 21, 13, 21, 15
+// let a = 9
+// for(let i=1; i<=4; i++){
+//     document.writeln("21" +" "+ a)
+//     a+=2;
+
+// }
+
+//q7. 58, 52, 46, 40, 34,28
+// let a = 58
+// for(let i=1; i<=6; i++){
+//     document.writeln(a)
+//     a-=6;
+
+// }
+
+//q8 . 3, 4, 7, 8, 11, 12 , 15
+// let a = 3
+// for(let i=1; i<=7; i++)
+// {
+//     document.writeln(a)
+//     if(a%2==0){
+//         a+=3
+//     }else{
+//         a+=1
+//     }
+// }
+
+//q9.  8, 22, 8, 28, 8 , 34 , 8 , 40
+// let a = 22
+//  for(let i=1; i<=4; i++){
+//      document.writeln("8" +" "+ a)
+//    a+=6;
+//  }
+
+//q10 . 31, 29, 24, 22, 17
+// let a = 31
+// for(let i=1; i<=4; i++){
+//     document.writeln(a)
+//    if(a%2==0){
+//         a-=5
+//     }else{
+//         a-=2
+//     }
+// }
+
+//Q11 . 1.5, 2.3, 3.1, 3.9, 4.7
+// let a = 1.5;
+// for(let i=1; i<=5; i++)
+// {
+//     document.writeln(a)
+//     a+=0.8
+// }
+
+//Q12 .14, 28, 20, 40, 32, 64
+// let a = 14
+// for(let i=1; i<=4; i++){
+//     document.writeln(a)
+//     a*=2
+//     document.writeln(a)
+//     a-=8
+
+// }
+
+//Q13 .  2, 4, 6, 8, 10, ...
+
+// for(let i=1; i<=12; i++)
+// {
+//     if(i%2==0)
+//     {
+//         document.writeln(i)
+//     }
+// }
+
+//Q14 .201, 202, 204, 207, 211
+// let a = 201;
+// for(let i=1; i<=5; i++)
+// {
+//     document.writeln(a)
+//     a=a+i;
+// }
+
+
+
+
+
+
+//Q15 . 544, 509, 474, 439 , 404
+// a = 544
+// for(let i=1; i<=5; i++)
+// {
+//     document.writeln(a)
+//     a-=35;
+// }
+
+
+
+//Q16 .  80, 10, 70, 15, 60,
+// let a = 80
+// let b = 10
+// for(let i=1; i<=4; i++){
+//     document.writeln(a+" "+b)
+//     a-=10;
+//     b+=5;
+// }
+
+
+//Q17 .2, 6, 18, 54, 162
+// let a = 2
+// for(let i=1; i<=5; i++)
+// {
+//     document.writeln(a)
+//     a*=3;
+// }
+
+
+
+//Q18.  5.2, 4.8, 4.4, 4 , 3.6
+// let a = 5.2;
+// for(let i=1; i<=5; i++)
+// {
+//     document.writeln(a)
+//     a-=0.4;
+// }
+
+//q19. 8, 6, 9, 23, 87 , 429
+// a = 8
+// b = 2
+// for(let i=1; i<=6; i++){
+//     document.writeln(a)
+//     b++;
+//     a=a*i-b;
+//     a++;
+
+// }
