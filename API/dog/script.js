0b35468d5dd559680e9b243637fac5bd7ee169ca
yatin modi <9756yatinmodi@gmail.com>
2025-07-09 14:20:27 +0530

    Add files via upload

---
+++ API/dog/script.js
@@ -0,0 +1,15 @@
+const img=document.getElementById("img");
+const btn=document.getElementById("btn");
+
+function displayDog(){
+fetch("https://dog.ceo/api/breeds/image/random")
+.then((Response)=>Response.json())
+.then((dog)=>{
+img.src=dog.message
+});
+}
+displayDog();
+
+btn.addEventListener("click",()=>{
+    displayDog();
+});
